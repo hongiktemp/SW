7998cb0b944cb9cab5abe079a835413e48bdf8e7
lim <lim@192.168.219.113>
2025-05-26 22:50:50 +0900

    check Jenkins

--- Test2/src/student/StudentManager.java
+++ Test2/src/student/StudentManager.java
@@ -6,7 +6,7 @@ import java.util.Set;
 public class StudentManager {
 	private Set<String> students = new HashSet<>();
 	
-	//학생 추가
+	// 학생 추가
     public void addStudent(String name) {
         if (students.contains(name)) {
             throw new IllegalArgumentException("이미 존재하는 학생입니다: " + name);
@@ -14,7 +14,7 @@ public class StudentManager {
         students.add(name);
     }
     
-    //학생 제거
+    // 학생 제거
     public void removeStudent(String name) {
         if (!students.contains(name)) {
             throw new IllegalArgumentException("존재하지 않는 학생입니다: " + name);
@@ -22,7 +22,7 @@ public class StudentManager {
         students.remove(name);
     }
     
-    //학생 조회(boolean타입 반환)
+    // 학생 조회(boolean타입 반환)
     public boolean hasStudent(String name) {
         return students.contains(name);
     }
